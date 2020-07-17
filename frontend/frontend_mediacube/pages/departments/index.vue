879af4094031ae4f60a9e119754ebffbb00193be
ge2nadiy <ge2nadiy.k@gmail.com>
2020-07-17 22:05:15 +0300

    finished task

---
+++ frontend/frontend_mediacube/pages/departments/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Departments</h1>
+  <a href="" class="btn btn-primary pull-right"><i class="fa fa-plus-square-o"></i> Создать отдел</a>
+  <table class="table table-striped">
+    <thead>
+    <th>Название отдела</th>
+    <th>Кол-во сотрудников отдела</th>
+    <th>Макс ЗП среди сотрудников отдела</th>
+    <th>Действия</th>
+    </thead>
+    <tbody>
+    <!-- -->
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>
+        <a href="">Edit<i class="fa fa-edit"></i></a>
+      </td>
+    </tr>
+    <!--@empty -->
+
+    <!--@endforelse -->
+    </tbody>
+    <tfoot>
+    <tr>
+      <td colspan="4">
+        <ul class="pagination pull-right">
+          <!-- {{$departments->links()}} -->
+        </ul>
+      </td>
+    </tr>
+    </tfoot>
+  </table>
+</template>
---
+++ frontend/frontend_mediacube/pages/employees/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>Employees</h1>
+
+  <table class="table table-striped">
+    <thead>
+    <th>Имя</th>
+    <th>Фамилия</th>
+    <th>Отчество</th>
+    <th>Пол</th>
+    <th>Заработная плата</th>
+    <th>Названия отделов</th>
+    <th>Действия</th>
+    </thead>
+    <tbody>
+    <!--@forelse() -->
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td>
+        <a href="">dsad<i class="fa fa-edit"></i></a>
+      </td>
+    </tr>
+    <!--@empty -->
+
+    <!--@endforelse -->
+    </tbody>
+    <tfoot>
+    <tr>
+      <td colspan="7">
+        <ul class="pagination pull-right">
+          <!-- {{$employees->links()}} -->
+        </ul>
+      </td>
+    </tr>
+    </tfoot>
+  </table>
+</template>
